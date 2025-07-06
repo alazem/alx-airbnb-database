4bbe4bc4393c7f8c2788624100588091a6ea7ab6
Alazar Zemene <149241148+alazem@users.noreply.github.com>
2025-07-06 23:09:21 +0300

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -18,6 +18,28 @@ JOIN users ON bookings.user_id = users.id
 JOIN properties ON bookings.property_id = properties.id
 JOIN payments ON bookings.id = payments.booking_id;
 
+-- Analyze performance of the initial query using EXPLAIN
+EXPLAIN ANALYZE
+SELECT 
+    bookings.id AS booking_id,
+    bookings.start_date,
+    bookings.end_date,
+    users.id AS user_id,
+    users.name AS user_name,
+    users.email,
+    properties.id AS property_id,
+    properties.name AS property_name,
+    properties.location,
+    payments.id AS payment_id,
+    payments.amount,
+    payments.status
+FROM 
+    bookings
+JOIN users ON bookings.user_id = users.id
+JOIN properties ON bookings.property_id = properties.id
+JOIN payments ON bookings.id = payments.booking_id
+WHERE bookings.start_date >= '2024-01-01'
+  AND payments.status = 'confirmed';
 
 --optimized one
 
